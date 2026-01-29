800b0ba1a041c3c8a184227d338ca3ee15955378
Arjun Trivedi <arjun25trivedi@gmail.com>
2026-01-29 11:56:10 +0530

    Running sum with and without using java streams

--- src/com/example/stream/ComputeRunningSum.java
+++ src/com/example/stream/ComputeRunningSum.java
@@ -8,10 +8,29 @@ import java.util.stream.IntStream;
 public class ComputeRunningSum {
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-		List<Integer> list = IntStream.range(0, numbers.size())
+		List<Integer> runningSumList = IntStream.range(0, numbers.size())
 				.mapToObj(i -> numbers.subList(0, i + 1).stream()
 						.mapToInt(Integer::intValue).sum()).toList();
 
-		System.out.println(list);
+		System.out.println(runningSumList);
+		
+		int[] runningSum = new int[numbers.size()];
+		int length = runningSum.length;
+		int sum = 0;
+		for (int i = 0; i < length; i++) {
+			sum += numbers.get(i);
+			runningSum[i] = sum; 
+		}
+		System.out.println(Arrays.toString(runningSum));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
