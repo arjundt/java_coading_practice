df482e818c207e626e4391da5539c44db0ecee8d
Arjun Trivedi <arjun25trivedi@gmail.com>
2026-02-10 14:55:13 +0530

    Find first element from list

---
+++ src/com/example/stream/FindFirst.java
@@ -0,0 +1,12 @@
+package com.example.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindFirst {
+	public static void main(String[] args) {
+		List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
+		Integer firstInt = myList.stream().findFirst().get();
+		System.out.println(firstInt);
+	}
+}
