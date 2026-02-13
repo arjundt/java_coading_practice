3a26f088d8279c680120299e708fb4b993e029b5
Arjun Trivedi <arjun25trivedi@gmail.com>
2026-02-13 14:02:39 +0530

    sorting primitive arrays

--- src/com/example/stream/SortNumberList.java
+++ src/com/example/stream/SortNumberList.java
@@ -3,6 +3,7 @@ package com.example.stream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 //Sort a list of integers in descending order.
@@ -13,5 +14,19 @@ public class SortNumberList {
 				.stream().sorted(Comparator.reverseOrder())
 				.collect(Collectors.toCollection(ArrayList::new));
 		System.out.println(collect);
+		
+		int[] arr = new int[]{10,15,8,49,25,98,98,32,15};
+		List<Integer> listIncrement = Arrays.stream(arr)
+			.boxed()
+			.sorted(Comparator.reverseOrder())
+			.toList();
+		System.out.println(listIncrement);
+		
+		int[] sortedDesc = Arrays.stream(arr)
+		        .map(i -> -i)
+		        .sorted()
+		        .map(i -> -i)
+		        .toArray();
+		System.out.println(Arrays.toString(sortedDesc));
 	}
 }
