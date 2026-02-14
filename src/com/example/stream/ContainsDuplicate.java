a997c4d444100a60efe8d8ae6a98406ef3ba040f
Arjun Trivedi <arjun25trivedi@gmail.com>
2026-02-15 00:35:57 +0530

    Program to check array contains duplicate

---
+++ src/com/example/stream/ContainsDuplicate.java
@@ -0,0 +1,27 @@
+package com.example.stream;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+//Given an integer array nums, return true if any value appears 
+//at least twice in the array, and return false if every element is distinct.
+public class ContainsDuplicate {
+
+	public static void main(String[] args) {
+		int[] nums = new int[] {1, 2, 3, 2};
+		List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+		Set<Integer> set = new HashSet<>(list);
+		if (set.size() == list.size()) {
+			System.out.println("false");
+		}
+		System.out.println("false");
+
+		/* or can also try below way */
+		Set<Integer> setData = new HashSet<>();
+		boolean containsDuplicate = Arrays.stream(nums).anyMatch(num -> !setData.add(num));
+		System.out.println(containsDuplicate);
+	}
+}
