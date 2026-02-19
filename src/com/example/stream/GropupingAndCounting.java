90a3cb93729771feee03da3f364180e4514af23c
Arjun Trivedi <arjun25trivedi@gmail.com>
2026-02-19 18:03:18 +0530

    find only duplicate elements with its count from the String ArrayList in Java8

--- src/com/example/stream/GropupingAndCounting.java
+++ src/com/example/stream/GropupingAndCounting.java
@@ -14,5 +14,11 @@ public class GropupingAndCounting {
 				.collect(Collectors.groupingBy(
 						Function.identity(), Collectors.counting()));
 		System.out.println(grouping);
+		
+//		find only duplicate elements with its count from the String ArrayList in Java8
+		Map<String, Long> onlyDuplicates = grouping.entrySet().stream()
+				.filter(entry -> entry.getValue() > 1)
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+		System.out.println(onlyDuplicates);
 	}
 }
