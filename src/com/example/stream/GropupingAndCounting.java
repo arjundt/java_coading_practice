f317c7915195aeb2100a8bfc935ed2fa31a151a6
Arjun Trivedi <arjun25trivedi@gmail.com>
2026-02-19 15:18:49 +0530

    count each element/word from the String ArrayList in Java8

---
+++ src/com/example/stream/GropupingAndCounting.java
@@ -0,0 +1,18 @@
+package com.example.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+//count each element/word from the String ArrayList in Java8
+public class GropupingAndCounting {
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("AA", "BB", "AA", "CC");
+		Map<String, Long> grouping = names.stream()
+				.collect(Collectors.groupingBy(
+						Function.identity(), Collectors.counting()));
+		System.out.println(grouping);
+	}
+}
