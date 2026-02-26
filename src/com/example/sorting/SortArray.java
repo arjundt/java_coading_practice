74c377c2bf33c002e7328bb302e41a68fd41cf0b
Arjun Trivedi <arjun25trivedi@gmail.com>
2026-02-26 23:24:03 +0530

    Modularized the sorting logic

--- src/com/example/sorting/SortArray.java
+++ src/com/example/sorting/SortArray.java
@@ -2,9 +2,14 @@ package com.example.sorting;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+public class SortArray {
 	public static void main(String[] args) {
 		int[] arr = {3, 2, 1};
+		arr = bubbleSort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static int[] bubbleSort(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0 + i; j < arr.length; j++) {
 				int temp;
@@ -15,6 +20,6 @@ public class BubbleSort {
 				}
 			}
 		}
-		System.out.println(Arrays.toString(arr));
+		return arr;
 	}
 }
