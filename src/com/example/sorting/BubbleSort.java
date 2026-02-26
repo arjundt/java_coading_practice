3bdecd6a2477a463b26cfbd4328ac25aa4a36ac9
Arjun Trivedi <arjunt@cdac.in>
2026-02-26 23:05:29 +0530

    Bubble sort

---
+++ src/com/example/sorting/BubbleSort.java
@@ -0,0 +1,21 @@
+package com.example.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] arr = {3, 2, 1};
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0 + i; j < arr.length; j++) {
+				int temp;
+				if(arr[i] > arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+}
