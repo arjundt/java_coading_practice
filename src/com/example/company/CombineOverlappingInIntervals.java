652438348f75b176477d5a3fe1b1e504b6e942ab
Arjun Trivedi <arjun25trivedi@gmail.com>
2026-03-01 23:26:16 +0530

    Merge overlapping in the intervals

--- src/com/example/company/CombineOverlappingInIntervals.java
+++ src/com/example/company/CombineOverlappingInIntervals.java
@@ -12,7 +12,7 @@ import java.util.List;
 //		Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 //		Output: [[1,6],[8,10],[15,18]]
 //		Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6]
-public class CapegeminiTest {
+public class CombineOverlappingInIntervals {
 	public static void main(String[] args) {
 		int[][] intervals = {{1,3}, {2,6}, {8,10}, {15,18}};
 		int[][] mergedIntervals = mergeIntervals(intervals);
