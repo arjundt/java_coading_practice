7fbf5d3dc5220e833c2cca53fec883644fcc931d
Arjun Trivedi <arjun25trivedi@gmail.com>
2026-03-03 19:13:42 +0530

    Pattern matcing with instanceof operator

---
+++ src/com/example/java17/PatternMatchingWithInstanceOf.java
@@ -0,0 +1,12 @@
+package com.example.java17;
+
+public class PatternMatchingWithInstanceOf {
+
+	public static void main(String[] args) {
+		var ss = "Arjun";
+		if(ss instanceof String str) {
+			System.out.println("String is: " + str);
+		}
+	}
+
+}
