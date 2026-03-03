565092253c1151f6eadb8a8276e5c1f3c1a36f8a
Arjun Trivedi <arjun25trivedi@gmail.com>
2026-03-03 19:14:13 +0530

    Formatting changes

--- src/com/example/java17/PatternMatchingWithInstanceOf.java
+++ src/com/example/java17/PatternMatchingWithInstanceOf.java
@@ -1,12 +1,10 @@
 package com.example.java17;
 
 public class PatternMatchingWithInstanceOf {
-
 	public static void main(String[] args) {
 		var ss = "Arjun";
 		if(ss instanceof String str) {
 			System.out.println("String is: " + str);
 		}
 	}
-
 }
